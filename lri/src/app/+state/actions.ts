e1cde10cdc543d93992c1e3f92c87bac2355ade0
evheniichuhai <evgeniychuhay@gmail.com>
2022-02-14 11:08:32 +0300

    deleted unused parts of code

--- lri/src/app/+state/actions.ts
+++ lri/src/app/+state/actions.ts
@@ -1,11 +1,5 @@
-import {GeoData} from "./state";
 
 export class LoadGeoData {
   static readonly type = '[GeoAPI] Load Geo Data';
 }
 
-export class SetGeoData {
-  static readonly type = '[GeoAPI] Set Geo Data'
-  constructor(public payload: GeoData ) {
-  }
-}
